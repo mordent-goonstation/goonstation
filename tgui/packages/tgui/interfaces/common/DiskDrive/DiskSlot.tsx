af16167b1e59a90ab4e523604ac8cdd5a9b8fbdc
Mordent <mordent.goonstation@gmail.com>
2025-09-06 00:47:28 -0400

    Code tidy up of DiskRack
    - Separate DiskDrive component
    - Add an eject button a la IRL drives

---
+++ tgui/packages/tgui/interfaces/common/DiskDrive/DiskSlot.tsx
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * @copyright 2025
+ * @author LeahTheTech (https://github.com/TobleroneSwordfish)
+ * @author Mordent (https://github.com/mordent-goonstation)
+ * @license MIT
+ */
+
+import { Button } from 'tgui-core/components';
+
+interface DiskSlotProps {
+  onClick?: () => void;
+}
+
+const style = {};
+
+export function DiskSlot(props: DiskSlotProps) {
+  const { onClick } = props;
+  return (
+    <Button
+      disabled={!onClick}
+      fluid
+      onClick={onClick}
+      style={style}
+      textAlign="center"
+      height="100%"
+      verticalAlignContent="middle"
+    >
+      Empty Slot
+    </Button>
+  );
+}
